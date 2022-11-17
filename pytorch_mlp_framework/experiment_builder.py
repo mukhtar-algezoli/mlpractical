880492c15e1621297f31baa01c6e02f15d2b9544
mukhtar-algezoli <38084259+mukhtar-algezoli@users.noreply.github.com>
2022-11-17 13:02:45 +0000

    debuging gradients from generator

--- pytorch_mlp_framework/experiment_builder.py
+++ pytorch_mlp_framework/experiment_builder.py
@@ -156,7 +156,9 @@ class ExperimentBuilder(nn.Module):
         ########################################
         # print("i am here")
         for para in named_parameters:
-            print(para[1].grad)
+            # print(para[1].grad)
+            print(np.mean(para[1].grad))
+
         #     print(para)
         #     # print(para[1])
         #     # print(para[1].detach().numpy())
